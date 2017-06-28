943398c41a9b25dad88971f1acd45bca9435978c
EndlessRival <7658937@qq.com>
2017-06-28 12:10:38 +0800

    Ray.cs

---
+++ project/RayTracingInOneWeekend/Ray.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RayTracingInOneWeekend
+{
+    class Ray
+    {
+        Vector3 A, B;
+        public Ray() { }
+        public Ray(Vector3 a,Vector3 b) { A = a; B = b; }
+        public Vector3 origin() { return A; }
+        public Vector3 direction() { return B; }
+        public Vector3 point_at_parameter(float t) { return A + B*t; }
+    }
+}
